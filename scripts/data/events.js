fcc04cf8ce14e6e8f225f85942014ea2b4d43eb8
Manuel Boillod <manuel.boillod@zenika.com>
2013-10-03 18:37:33 +0200

    Affichage de données html dans subject.abstract et speaker.bio

--- scripts/data/events.js
+++ scripts/data/events.js
@@ -20,11 +20,10 @@ var njEvents = [
         length: '1h30',
         type: 'talk',
         intro: 'par Nicolas Deloof, en avant-première avant Devoxx.be, dont nous avons gardé la description originale.',
-        abstract: [
-          'Le sujet lui sera bien en français - mais le speaker est trop flemmard - enfin c\'est lui qui le dit :p.',
-          'Cloud "Platform as a Service" promise to host your application without a change, but things are not so simple and you can\'t deploy your legacy EJB 1.0 application to the Cloud without some refactoring.',
-          'During this session, I will explain some architecture patterns to apply to your new developments, and the possible refactoring or platform services that can help to get your existing application migrated without major changes, so that your transition to the cloud is as smooth as possible. To avoid a pure theoretical talk, I\'ll share experience about migrating devoxx application from traditional hosting to cloud.'
-        ],
+        abstract:
+          '<p>Le sujet lui sera bien en français - mais le speaker est trop flemmard - enfin c\'est lui qui le dit :p.</p>' +
+          '<p>Cloud "Platform as a Service" promise to host your application without a change, but things are not so simple and you can\'t deploy your legacy EJB 1.0 application to the Cloud without some refactoring.</p>' +
+          '<p>During this session, I will explain some architecture patterns to apply to your new developments, and the possible refactoring or platform services that can help to get your existing application migrated without major changes, so that your transition to the cloud is as smooth as possible. To avoid a pure theoretical talk, I\'ll share experience about migrating devoxx application from traditional hosting to cloud.</p>',
         speakers: ['nicolas_deloof']
       }
     ]
@@ -41,7 +40,7 @@ var njEvents = [
         title: 'Sujet 1',
         length: '20 min',
         type: 'quickie',
-        abstract: ['abstract 1'],
+        abstract: 'abstract 1',
         speakers: []
       },
       {
