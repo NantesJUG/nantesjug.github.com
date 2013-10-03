63c8333fa2105dc1dc1aebb4ff7324e63516eae9
Manuel Boillod <manuel.boillod@zenika.com>
2013-10-03 20:10:06 +0200

    Affichage des locations

--- scripts/data/places.js
+++ scripts/data/places.js
@@ -3,13 +3,15 @@ var njPlaces = [
   {
     id: 'epsi',
     name: 'EPSI Nantes',
-    address: '2 Rue Fénelon, 44000 Nantes',
+    address: '2 Rue Fénelon',
+    city: '44000 Nantes',
     maps: 'http://goo.gl/maps/EO03M'
   },
   {
     id: 'cantine',
     name: 'Cantine numérique',
-    address: 'cantine, 44000 Nantes',
-    maps: 'http://goo.gl/maps/EO03M'
+    address: '11 Impasse Juton',
+    city: '44000 Nantes',
+    maps: 'http://goo.gl/maps/0Swz1'
   }
 ];
