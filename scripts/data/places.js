d65b98acedeea50aebc4506d52406f4c7d42d8de
Manuel Boillod <manuel.boillod@zenika.com>
2013-11-19 13:45:13 +0100

    Ajout de la capacité de chaque salle

--- scripts/data/places.js
+++ scripts/data/places.js
@@ -5,34 +5,37 @@ var njPlaces = [
     name: 'EPSI Nantes',
     address: '2 Rue Fénelon',
     city: '44000 Nantes',
-    maps: 'http://goo.gl/maps/EO03M'
+    maps: 'http://goo.gl/maps/EO03M',
+    capacity: 70
   },
   {
     id: 'cantine',
     name: 'Cantine numérique',
     address: '11 Impasse Juton',
     city: '44000 Nantes',
-    maps: 'http://goo.gl/maps/0Swz1'
+    maps: 'http://goo.gl/maps/0Swz1',
+    capacity: 40
   },
   {
     id: 'netapsys',
     name: 'Netapsys',
     address: '17 Rue Sanlecque',
     city: '44000 Nantes',
-    maps: 'http://goo.gl/maps/Ctujp'
+    capacity: 'http://goo.gl/maps/Ctujp'
   },
   {
     id: 'sup_info',
     name: 'Sup Info',
     address: '1 Rue Charron',
     city: '44800 Saint-Herblain',
-    maps: 'http://goo.gl/maps/YbjKy'
+    capacity: 'http://goo.gl/maps/YbjKy'
   },
   {
     id: 'epitech',
     name: 'Epitech',
     address: '16-18 Rue Flandres Dunkerque',
     city: '44100 Nantes',
-    maps: 'http://goo.gl/maps/DCQ1a'
+    maps: 'http://goo.gl/maps/DCQ1a',
+    capacity: 130
   }
 ];
