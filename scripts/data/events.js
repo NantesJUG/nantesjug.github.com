e15b58516d9392fefa3a952ec4d1b41b2b7c6364
Tugdual Grall <tgrall@users.noreply.github.com>
2014-02-28 16:38:25 +0100

    Ajout du lien d'enregistrement

--- scripts/data/events.js
+++ scripts/data/events.js
@@ -5,7 +5,7 @@ var njEvents = [
     title: 'Du Legacy Au Cloud en Moins d\'Une Heure',
     date: new Date(2014, 2, 17),//month begin at 0
     time: '19',
-    register: null,
+    register: 'http://guestlistapp.com/events/234840',
     place: 'epitech',
     subjects: [
       {
