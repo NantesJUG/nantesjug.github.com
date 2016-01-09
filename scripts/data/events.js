30d026510ccfe03071ba9d47f49e14a5aa3f0cb6
pierre <pierre.cosson@gmail.com>
2016-01-09 11:34:01 +0100

    Ajout du GuestList pour la soirée de Janvier 2016

--- scripts/data/events.js
+++ scripts/data/events.js
@@ -5,7 +5,7 @@ var njEvents = [
 	  title: "Orchestrating a Continuous Delivery Pipeline with Jenkins and Docker",
 	  date: new Date(2016, 0, 19), //month begin at 0
 	  time: '19',
-	  register: null,
+	  register: 'http://guestli.st/401376',
 	  place: 'hub_creatic',
 	  subjects: [
 	      {
