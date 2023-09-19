dad19d83f079319aeca0c2187d0792000c9bad3a
Manuel Boillod <boillodmanuel@gmail.com>
2023-09-19 12:46:22 +0200

    update sponsor logos

---
+++ images/parrains/resize.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+FILENAME="$1"
+BASENAME="${FILENAME%.*}"
+WIDTH=280
+HEIGHT=101
+TMP_PREFIX="__tmp_img"
+
+cp "$FILENAME" "${TMP_PREFIX}.png"
+
+# CROP
+mogrify -crop "${WIDTH}:${HEIGHT}" -format png "${TMP_PREFIX}.png" # generates 1 or 2 files if cropped
+if [ -f "${TMP_PREFIX}-0.png" ]; then
+    mv "${TMP_PREFIX}-0.png" "${TMP_PREFIX}.png"
+    rm "${TMP_PREFIX}-1.png"
+fi
+
+# RESIZE
+mogrify -resize "${WIDTH}x${HEIGHT}" -format png "${TMP_PREFIX}.png"
+
+# RENAME
+mv "${TMP_PREFIX}.png" "$BASENAME.png"
